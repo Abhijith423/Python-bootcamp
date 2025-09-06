699224aca32a8a1fd45d835e1a640f732477ebdf
Abhijith <abhijithemes@gmail.com>
2025-09-07 00:02:03 +0530

    mathfun.py

---
+++ mathfun.py
@@ -0,0 +1,25 @@
+#math functions in python
+
+import math
+
+a=min(2,1,23,16)  #min function
+print(a)
+
+b=max(67,134,156,12) #max function
+print(b)
+
+#exponentition using two numbers
+d=pow(3,3)
+print(d)
+
+f=math.pi
+print(f)
+
+s=math.gcd(5,12)
+print(s)
+
+s=math.lcm(5,7)
+print(s)
+
+a=math.sqrt(9)
+print(a)
