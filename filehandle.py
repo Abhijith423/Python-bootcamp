0c24ca11561b55bdf8a8caefd35cb714af308a20
Abhijith <abhijithemes@gmail.com>
2025-09-07 12:07:02 +0530

    Create filehandle.py

---
+++ filehandle.py
@@ -0,0 +1,11 @@
+#filehandling in python
+
+f=open("Sample.txt","a")     #using append a
+f.write("learn java programming")
+f.close()
+
+f=open("Sample.txt","w")
+f.write("learn java programming")
+f.close()
+
+
